ef270a47e9cb0f680fbf60f5012566f7a0ef1658
Admin <tuchinsky.gs@ptmk.ru>
2018-01-18 12:16:59 +0300

    Добавил файл менеджер точек для unity

---
+++ WaypointManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WaypointManager : MonoBehaviour {
+    public Transform[] waypoints;
+    
+    void Start() {
+        waypoints = new Transform[transform.childCount];
+        int i = 0;
+        
+        foreach (Transform t in transform) {
+            waypoints[i++] = t;
+        }
+    }
+}
