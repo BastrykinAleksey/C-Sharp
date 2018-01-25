61be02c29b93484d0ee95dbe0a52bdbcde78ab5e
Admin <tuchinsky.gs@ptmk.ru>
2018-01-25 10:28:51 +0300

    add translateScript

---
+++ Unity3D/TranslateScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class TranslateScript : MonoBehaviour {
+    public float distancePerSecond;
+    
+    void Update() {
+        transform.Translate(0, 0, distancePerSecond * Time.deltaTime);
+    }
+}
