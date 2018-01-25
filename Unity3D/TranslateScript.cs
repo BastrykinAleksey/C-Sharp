3931ce0e991a06acdda548d829fe8e9343895bc3
Admin <tuchinsky.gs@ptmk.ru>
2018-01-25 10:36:34 +0300

    change translateScript

--- Unity3D/TranslateScript.cs
+++ Unity3D/TranslateScript.cs
@@ -5,6 +5,7 @@ public class TranslateScript : MonoBehaviour {
     public float distancePerSecond;
     
     void Update() {
-        transform.Translate(0, 0, distancePerSecond * Time.deltaTime);
+        transform.Translate(0, 0, distancePerSecond * Time.deltaTime); // distancePerSecond 
+        //transform.Translate(0, 0, distancePerFrame); // distancePerFrame
     }
 }
